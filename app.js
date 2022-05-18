642faa11e76a0c4332139290c30d04a2fae2ee4f
Juan Pablo Gavilán <jpgavilan@auter.cl>
2022-05-18 17:51:49 -0400

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js'
 
-import { onSnapshot, orderBy, query,getFirestore,collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js"
+import { onSnapshot, orderBy, query, getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js"
 
 
 const firebaseConfig = {
@@ -16,50 +16,55 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 let x = 1;
-const getQuestions = async () =>{
+let tb = document.getElementById('table');
+const getQuestions = async () => {
+    document.querySelectorAll("tbody").forEach(function(e){e.remove()})
     console.log("Obteneido datos");
-    const q = query(collection(db, 'preguntas'), orderBy('orden','desc'));
+    const q = query(collection(db, 'preguntas'), orderBy('orden', 'desc'));
     return getDocs(q);
     //console.log(onSnapshot(query(collection(db, "preguntas"), orderBy("orden"))));
 }
-const tb = document.getElementById('table');
-const questions = await getQuestions();
 
-console.log(questions);
-let largo = questions.size;
-questions.forEach(doc => {
-   
-    console.log(doc.data());
-    var newRow   = tb.insertRow(1);
 
-    // Inserta una celda en la fila, en el índice 0
-    var newCell  = newRow.insertCell(0);
-    var newCell1  = newRow.insertCell(1);
-    var newCell2  = newRow.insertCell(2);
-  
-    // Añade un nodo de texto a la celda
-    var newText  = document.createTextNode(doc.data().orden);
-    var newText1  = document.createTextNode(doc.data().pregunta);
-    let resp = '';
-    switch(doc.data().respuesta){
-        case 'texto' :
-            resp = 'Texto';
-        break;
-        case 'numero' :
-            resp = 'Númerico';
-        break;
-        case 'seleccion' :
-            resp = 'Selección';
-        break;
-        default:
-            resp = 'N/A'
-            break;
-    }
-    var newText2  = document.createTextNode(resp);
-    newCell.appendChild(newText);
-    newCell1.appendChild(newText1);
-    newCell2.appendChild(newText2);
-})
+const setQuestions = async () => {
+    const questions = await getQuestions();
+    console.log(questions);
+    questions.forEach(doc => {
+
+        console.log(doc.data());
+        var newRow = tb.insertRow(1);
+
+        // Inserta una celda en la fila, en el índice 0
+        var newCell = newRow.insertCell(0);
+        var newCell1 = newRow.insertCell(1);
+        var newCell2 = newRow.insertCell(2);
+
+        // Añade un nodo de texto a la celda
+        var newText = document.createTextNode(doc.data().orden);
+        var newText1 = document.createTextNode(doc.data().pregunta);
+        let resp = '';
+        switch (doc.data().respuesta) {
+            case 'texto':
+                resp = 'Texto';
+                break;
+            case 'numero':
+                resp = 'Númerico';
+                break;
+            case 'seleccion':
+                resp = 'Selección';
+                break;
+            default:
+                resp = 'N/A'
+                break;
+        }
+        var newText2 = document.createTextNode(resp);
+        newCell.appendChild(newText);
+        newCell1.appendChild(newText1);
+        newCell2.appendChild(newText2);
+    })
+}
+
+await setQuestions()
 
 document.getElementById("btn").onclick = function () { savePoll() };
 
@@ -70,31 +75,33 @@ async function savePoll() {
     try {
         const docRef = await addDoc(collection(db, "preguntas"), {
             pregunta: document.getElementById('pregunta').value,
-            respuesta: document.getElementById('respuesta').value
+            respuesta: document.getElementById('respuesta').value,
+            orden : 7
         });
         console.log("Document written with ID: ", docRef.id);
 
         const docRef2 = await addDoc(collection(db, "encuestas"), {
-            registros : [
+            registros: [
                 {
-                    pregunta : 1,
-                    respuesta : "Juan Pablo"
+                    pregunta: 1,
+                    respuesta: "Juan Pablo"
                 },
                 {
-                    pregunta : 2,
-                    respuesta : 29
+                    pregunta: 2,
+                    respuesta: 29
                 },
                 {
-                    pregunta : 3,
-                    respuesta : "C"
+                    pregunta: 3,
+                    respuesta: "C"
                 },
                 {
-                    pregunta : 4,
-                    respuesta : "A"
+                    pregunta: 4,
+                    respuesta: "A"
                 },
             ]
         });
         console.log("Document written with ID: ", docRef2.id);
+        await setQuestions()
     } catch (e) {
         console.error("Error adding document: ", e);
     }
